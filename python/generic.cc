ryoteru0220/improved-system

commit 81a5896ee410c62ccf140b48142a5358a07331ca

Issue warnings as apt_pkg.Warning instead of discarding when there is no error
APT stores a stack of errors, warnings and notices. Previously
we concatenated all errors and warnings and then raised an
exception. If there were no errors, we just silently discarded
warnings though.

This makes python-apt issue warnings instead, using apt_pkg.Warning.

Example, run in a directory _apt has no write access to:

    $ cat a.py
    import apt_pkg
    apt_pkg.init()
    acq=apt_pkg.Acquire()
    fil=apt_pkg.AcquireFile(acq, uri="https://jak-linux.org", destfile="test.html")
    acq.run()
    $ sudo python3 a.py
    $ cd lib.linux-x86_64-cpython-311/
    $ sudo python3 a.py
    /home/jak/Projects/Debian/python-apt/build/lib.linux-x86_64-cpython-311/a.py:5: Warning: W:Download is performed unsandboxed as root as file 'test.html' couldn't be accessed by user '_apt'. - pkgAcquire::Run (13: Permission denied)
      acq.run()

python/generic.cc
@@ -20,33 +20,40 @@ using namespace std;
 /* We throw away all warnings and only propogate the first error. */
 PyObject *HandleErrors(PyObject *Res)
 {
-   if (_error->PendingError() == false)
-   {
-      // Throw away warnings
-      _error->Discard();
-      return Res;
-   }
-
    if (Res != 0) {
-      Py_DECREF(Res);
    }
 
    string Err;
    int errcnt = 0;
+   int wrncnt = 0;
    while (_error->empty() == false)
    {
       string Msg;
       bool Type = _error->PopMessage(Msg);
-      if (errcnt > 0)
+      if (errcnt > 0 || wrncnt > 0)
          Err.append(", ");
       Err.append((Type == true ? "E:" : "W:"));
       Err.append(Msg);
-      ++errcnt;
+      if (Type)
+          ++errcnt;
+      else
+          ++wrncnt;
    }
-   if (errcnt == 0)
-      Err = "Internal Error";
-   PyErr_SetString(PyAptError,Err.c_str());
-   return 0;
+   if (errcnt > 0)
+   {
+       PyErr_SetString(PyAptError,Err.c_str());
+       goto err;
+   }
+   else if (wrncnt > 0)
+   {
+       if (PyErr_WarnEx(PyAptWarning, Err.c_str(), 1) == -1)
+           goto err;
+   }
+
+   return Res;
+err:
+    Py_DECREF(Res);
+    return nullptr;
 }
 
 									/*}}}*/

python/apt_pkgmodule.cc
@@ -43,6 +43,13 @@ static char PyAptError_Doc[] =
 
 PyObject *PyAptError;
 
+static char PyAptWarning_Doc[] =
+   "Exception class for most python-apt warnings.\n"
+   "\n"
+   ".. versionadded:: 2.7";
+
+PyObject *PyAptWarning;
+
 static char PyAptCacheMismatchError_Doc[] =
    "Raised when passing an object from a different cache to\n"
    ":class:`apt_pkg.DepCache` methods\n\n"
@@ -885,6 +892,9 @@ extern "C" void initapt_pkg()
    if (PyType_Ready(&PyCacheFile_Type) == -1) INIT_ERROR;
    PyAptError = PyErr_NewExceptionWithDoc("apt_pkg.Error", PyAptError_Doc, PyExc_SystemError, NULL);
    if (PyAptError == NULL)
+      INIT_ERROR;
+   PyAptWarning = PyErr_NewExceptionWithDoc("apt_pkg.Warning", PyAptWarning_Doc, PyExc_Warning, NULL);
+   if (PyAptWarning == NULL)
       INIT_ERROR;
 
    PyAptCacheMismatchError = PyErr_NewExceptionWithDoc("apt_pkg.CacheMismatchError", PyAptCacheMismatchError_Doc, PyExc_ValueError, NULL);

python/generic.h
@@ -37,6 +37,7 @@
  * Exception class for almost all Python errors
  */
 extern PyObject *PyAptError;
+extern PyObject *PyAptWarning;
 /**
  * Exception class for invalidated cache objects.
  */

commit 735ff09c8688fb2bb5f984e439ac222dfd97c76b

Handle null pointer deference in error handler
The commit introducing support for warnings accidentally moved the Res
check outside of the if when it moved it to the end, causing a crash.

Fixes: 81a5896ee410c62ccf140b48142a5358a07331ca
       ("Issue warnings as apt_pkg.Warning instead of discarding when
       there is no error")
Closes: #1058904

python/generic.cc
@@ -20,9 +20,6 @@ using namespace std;
 /* We throw away all warnings and only propogate the first error. */
 PyObject *HandleErrors(PyObject *Res)
 {
-   if (Res != 0) {
-   }
-
    string Err;
    int errcnt = 0;
    int wrncnt = 0;
@@ -52,7 +49,9 @@ PyObject *HandleErrors(PyObject *Res)
 
    return Res;
 err:
-    Py_DECREF(Res);
+   if (Res != 0)
+      Py_DECREF(Res);
+
     return nullptr;
 }
 